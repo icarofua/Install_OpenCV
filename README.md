8c8cd9cafdcaebdba03636d3651e1a85cdbe4690
Icaro Oliveira de Oliveira <icarofua@gmail.com>
2016-01-20 16:09:36 -0200

    installation script

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# INSTALL_OPENCV
+# Install_OpenCV
+
+shell scripts to install github version of OpenCV with OpenCV-contrib in different distributions of Linux, MacOSX.
+In the file, you can edit the cmake options and finally run the command below.
+```
+`$ ./opencv_latest.sh
+```
---
+++ opencv_latest.sh
@@ -0,0 +1,51 @@
+if [ -d "opencv" ]; then
+	cd opencv
+	git pull
+	cd ..
+else
+	git clone https://github.com/Itseez/opencv.git
+fi
+
+if [ -d "opencv_contrib" ]; then
+	cd opencv_contrib
+        git pull
+	cd ..
+else
+        git clone https://github.com/Itseez/opencv_contrib.git                                
+fi
+
+_cmakeopts=('-D CMAKE_BUILD_TYPE=Release'
+            '-D CMAKE_INSTALL_PREFIX=/usr'
+            '-D PYTHON_EXECUTABLE=/usr/bin/python'
+            '-D CMAKE_SKIP_RPATH=ON'
+            '-D BUILD_TESTS=OFF'
+            '-D ENABLE_SSE=ON'
+            '-D ENABLE_SSE2=ON'
+            '-D ENABLE_SSE3=ON'
+            '-D ENABLE_SSSE3=ON'
+            '-D ENABLE_SSE41=ON'
+            '-D ENABLE_SSE42=ON'
+            '-D BUILD_EXAMPLES=OFF'
+            '-D INSTALL_C_EXAMPLES=OFF'
+            '-D INSTALL_PYTHON_EXAMPLES=OFF'
+            '-D WITH_XINE=OFF'
+            '-D WITH_QT=OFF'
+            '-D WITH_QT_OPENGL=OFF'
+            '-D WITH_UNICAP=OFF'
+            '-D WITH_PVAPI=OFF'
+            '-D WITH_OPENNI=OFF'
+            '-D WITH_TBB=OFF'
+            '-D WITH_IPP=OFF'
+            '-D WITH_CUDA=OFF'
+	    '-D OPENCV_EXTRA_MODULES_PATH=../../opencv_contrib/modules'
+            '-D USE_FAST_MATH=ON')
+
+cd opencv
+if [ -d "build" ]; then
+	rm -rf build
+fi
+mkdir build
+cd build
+cmake ${_cmakeopts[@]} ..
+make
+sudo make install
